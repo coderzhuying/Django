1dffaea9bc1956786cb0515856d587d79b4df554
contodo <13289265706@163.com>
2018-06-07 15:41:03 +0800

    跟老齐学Django第一章

---
+++ blogone/blog/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BlogArticles',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=300)),
+                ('body', models.TextField()),
+                ('publish', models.DateTimeField(default=django.utils.timezone.now)),
+                ('author', models.ForeignKey(related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ('-publish',),
+            },
+        ),
+    ]
---
+++ blogone/blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from .import views
+import blog
+
+urlpatterns=[
+    url(r'^$',views.blog_title,name='blog_title'),
+    url(r'(.+)$',views.blog_article,name="blog_detail")
+]
+
+handler404 = blog.views.page_not_found
